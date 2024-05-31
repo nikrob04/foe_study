816f9d22e0a60b3e684d6f43ed4380334dc9611f
Никитенко Илья <nikrob0494@gmail.com>
2024-05-31 16:41:54 +0700

    Аналоговые часы

--- lab8/time.js
+++ lab8/time.js
@@ -1,13 +1,16 @@
-function showTime () {
+function showTime() {
     let today = new Date();
     let currentTime = today.toLocaleTimeString('ru-RU');
     document.getElementById('time').innerHTML = currentTime;
 }
-setInterval(showTime, 1000)
+setInterval(showTime, 1000);
 
-function showSec () {
+function showSec() {
     let today = new Date();
     let currentsec = today.getSeconds();
     document.getElementById('timeSec').innerHTML = currentsec;
+
+    let degrees = currentsec * 6;
+    document.getElementById('arrow').style.transform = `rotate(${degrees}deg)`;
 }
-setInterval (showSec, 1000)
+setInterval(showSec, 1000);
